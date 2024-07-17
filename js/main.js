899c9667d344d5ae32a54daa8ff83fcd9eb2cb63
Matheus Nunes <matheusjobs04@gmail.com>
2024-07-17 19:50:46 -0300

    Funcionalidades finalizadas

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ const pokemonImage = document.querySelector('.pokemon_img')
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input_search')
+const next = document.querySelector('.btn-next')
+const prev = document.querySelector('.btn-prev')
 
+let searchPokemon = 1;
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -19,11 +22,14 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if(data){
+    pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
     input.value='';
+    searchPokemon = data.id;
     } else{
+        pokemonImage.style.display ='none'
         pokemonName.innerHTML = 'Not found';
         pokemonNumber.innerHTML = '';
     }
@@ -34,3 +40,17 @@ form.addEventListener('submit', (event) =>{
     renderPokemon(input.value.toLowerCase());
 
 })
+
+prev.addEventListener('click', () =>{
+    if(searchPokemon > 1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    }
+})
+
+next.addEventListener('click', () =>{
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon)
